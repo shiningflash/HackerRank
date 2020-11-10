276cf4d509fe04f72a203ec4454510893d3de4ab
Amirul Islam <amirulislam.bracu@gmail.com>
2020-11-11 00:28:41 +0600

    added

---
+++ Java/java_lambda_expression.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+import java.io.*;
+import java.util.*;
+
+interface PerformOperation {
+	boolean check(int a);
+}
+
+class MyMath {
+	public static boolean checker(PerformOperation p, int num) {
+		return p.check(num);
+	}
+	public static PerformOperation isOdd() {
+		return n -> (n & 1) == 1;
+	}
+	public static PerformOperation isPrime() {
+		return n -> {
+			if (n < 2) return false;
+			for (int i = 2; i * i <= n; i++) if (n % i == 0) return false;
+			return true;
+		};
+	}
+	public static PerformOperation isPalindrome() {
+		return n -> {
+			String s = Integer.toString(n);
+			for (int i = 0, j = s.length()-1; i < j; i++, j--) {
+				if (s.charAt(i) != s.charAt(j)) return false;
+			}
+			return true;
+		};
+	}
+}
+
+public class java_lambda_expression {
+
+	public static void main(String[] args) throws IOException {
+		MyMath ob = new MyMath();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		PerformOperation op;
+		boolean ret = false;
+		String ans = null;
+		while (T--> 0) {
+			String s = br.readLine().trim();
+			StringTokenizer st = new StringTokenizer(s);
+			int ch = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
+			if (ch == 1) {
+				op = ob.isOdd();
+				ret = ob.checker(op, num);
+				ans = (ret) ? "ODD" : "EVEN";
+			} else if (ch == 2) {
+				op = ob.isPrime();
+				ret = ob.checker(op, num);
+				ans = (ret) ? "PRIME" : "COMPOSITE";
+			} else if (ch == 3) {
+				op = ob.isPalindrome();
+				ret = ob.checker(op, num);
+				ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+
+			}
+			System.out.println(ans);
+		}
+	}
+}
