743f8c7bb6ce3197d8196491083b60c9f2a1a31c
Amirul Islam <amirulislamalmamun@gmail.com>
2021-11-18 20:10:26 +0600

    updated

---
+++ Python/athlete_sort_2.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+
+def athlete_sort(n, m, arr, k):
+    return sorted(arr, key=lambda x: x[k])
+
+
+def print_arr(n, m, arr):
+    for x in arr:
+        print(' '.join(list(map(str, x))))
+
+
+if __name__ == '__main__':
+    nm = input().split()
+    n = int(nm[0])
+    m = int(nm[1])
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+    k = int(input())
+    sorted_arr = athlete_sort(n, m, arr, k)
+    print_arr(n, m, sorted_arr)
